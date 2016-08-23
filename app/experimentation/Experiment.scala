4c73451f21a3f03484d4d99e1632edcf2b6d902a
Michael Seydel <micseydel@gmail.com>
2016-08-22 23:29:06 -0700

    Primitive but working player using the server.
    Couldn't figure out how to do dependencies correctly, so they're a softlink into the app with a .gitignore entry for them.
    
    Added the following endpoints:
    waitingFor - what input is being waited for
    choose - choose an input from waitingFor
    viewQueue - should generally be empty, but is useful for debugging
    viewField - needs tidying up, but is how the field is viewed

--- app/experimentation/Experiment.scala
+++ app/experimentation/Experiment.scala
@@ -1,10 +1,12 @@
 package experimentation
 
-import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue}
 import javax.inject.{Inject, Singleton}
 
 import play.api.mvc.{Action, Controller}
 import services.Counter
+import yugioh._
+import yugioh.action.DefaultActionModuleComponent
+import yugioh.events.DefaultEventsModuleComponent
 
 
 /**
@@ -14,29 +16,54 @@ import services.Counter
   * object is injected by the Guice dependency injection system.
   */
 @Singleton
-class ExperimentationController @Inject() () extends Controller {
+class ExperimentationController @Inject() extends Controller {
 
   /**
     * Create an action that responds with the [[Counter]]'s current
     * count. The result is plain text. This `Action` is mapped to
     * `GET /count` requests by an entry in the `routes` config file.
     */
-  def check = Action { Ok(Experiment.hello) }
+  def waitingFor = Action {
+    Ok(Main.player1.waitingFor)
+  }
 
-  def set(setTo: String) = Action { Experiment.queue.put(setTo); Ok("set to " + setTo) }
+  def choose(choice: String) = Action {
+    Main.player1.queue.put(choice); Ok(s"received $choice")
+  }
 
+  def viewQueue = Action {
+    Ok(s"Queue: ${Main.player1.queue}")
+  }
+
+  def viewField = Action {
+    Ok(s"Field:\n\n${Main.player1.fieldStr}")
+  }
 }
 
-object Experiment {
-  var hello = "start"
+object Main extends DefaultPlayGameComponent
+  with DefaultEventsModuleComponent
+  with DefaultBattlePhaseModuleComponent
+  with DefaultFieldModuleComponent
+  with DefaultPhaseModuleComponent
+  with DefaultActionModuleComponent {
 
-  val queue: BlockingQueue[String] = new LinkedBlockingQueue[String]()
+  val player1 = new ThroughServerPlayer("Human")(eventsModule, fieldModule)
+  val player2 = new PassivePlayer()(fieldModule)
+
+  override val Players = (player1, player2)
+
+  def main(): Unit = {
+    try {
+      playGame.mainLoop()
+    } catch {
+      case gameLoss: GameLoss =>
+        println(s"${gameLoss.loser} has lost! " + gameLoss)
+    }
+  }
 
   val thread = new Thread(new Runnable {
     def run() {
-      while (true) {
-        hello = queue.take()
-      }
+      main()
     }
   })
   thread.start()
